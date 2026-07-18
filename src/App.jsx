0bc2ae7ccec05615578f4f13faff7f18651e5056
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-18 16:02:29 -0400

    create Footer component and render footer navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+
+      <main>
+        
+      </main>
+
+      <Footer />
+    </>
+  );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import Nav from "./Nav.jsx";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Nav location="footer" />
+
+      <p className="copyright">
+        &copy; 2013 Valet Industries, Inc.
+      </p>
+    </footer>
+  );
+}
